d9bf38447d79bf9bb2431560a8f15ca54ca817c5
Amit Raj <49913746+amit1884@users.noreply.github.com>
2020-01-21 21:55:52 +0530

    loader

--- index.php
+++ index.php
@@ -94,22 +94,9 @@
     margin-left:2%;
   }
 
-  /* content {
-	 height: 100%;
-	 position: relative;
-	 z-index: 1;
-	 background-color: #d2e1ec;
-	 background-image: linear-gradient(to bottom, #bbcfe1 0%, #e8f2f6 80%);
-	 overflow: hidden;
-}
- .snow {
-	 position: absolute;
-	 top: 0;
-	 left: 0;
-	 pointer-events: none;
-	 z-index: 20;
-} */
-
+  .abt-foo{
+    display:none;
+  }
 
   #scroll_id {
     background: rgb(142, 193, 218);
@@ -122,12 +109,10 @@
   </style>
   </head>
   <body>
-  <!-- <div class="content">
-  <canvas class="snow" id="snow">
-  </canvas> -->
-  <!-- <div id ="loader-wrapper">
-<img src="svg\copper-loader.gif">
-</div> -->
+
+  <div id ="loader-wrapper">
+<?php include_once 'loader.php';?>
+</div>
 <br><br>
   <div class ="main-header-20">
   <div class ="head" style ="margin-left:15%;">
@@ -252,88 +237,6 @@ $(window).load(function(){
 
 });
   </script>
-  <!-- <script>
-  (function() {
-	function ready(fn) {
-		if (document.readyState != 'loading'){
-			fn();
-		} else {
-			document.addEventListener('DOMContentLoaded', fn);
-		}
-	}
-	
-	function makeSnow(el) {
-		var ctx = el.getContext('2d');
-		var width = 0;
-		var height = 0;
-		var particles = [];
-		
-		var Particle = function() {
-			this.x = this.y = this.dx = this.dy = 0;
-			this.reset();
-		}
-		
-		Particle.prototype.reset = function() {
-			this.y = Math.random() * height;
-			this.x = Math.random() * width;
-			this.dx = (Math.random() * 1) - 0.5;
-			this.dy = (Math.random() * 0.5) + 0.5;
-		}
-		
-		function createParticles(count) {
-			if (count != particles.length) {
-				particles = [];
-				for (var i = 0; i < count; i++) {
-					particles.push(new Particle());
-				}
-			}
-		}
-				
-		function onResize() {
-			width = window.innerWidth;
-			height = window.innerHeight;
-			el.width = width;
-			el.height = height;
-			
-			createParticles((width * height) / 10000);
-		}
-		
-		function updateParticles() {
-			ctx.clearRect(0, 0, width, height);
-			ctx.fillStyle = '#f6f9fa';
-			
-			particles.forEach(function(particle) {
-				particle.y += particle.dy;
-				particle.x += particle.dx;
-				
-				if (particle.y > height) {
-					particle.y = 0;
-				}
-				
-				if (particle.x > width) {
-					particle.reset();
-					particle.y = 0;
-				}
-				
-				ctx.beginPath();
-				ctx.arc(particle.x, particle.y, 5, 0, Math.PI * 2, false);
-				ctx.fill();
-			});
-			
-			window.requestAnimationFrame(updateParticles);
-		}
-		
-		onResize();
-		updateParticles();
-		
-		window.addEventListener('resize', onResize);
-	}
-	
-	ready(function() {
-		var canvas = document.getElementById('snow');
-		makeSnow(canvas);
-	});
-})();
-  </script> -->
+  
 </body>
 </html>
